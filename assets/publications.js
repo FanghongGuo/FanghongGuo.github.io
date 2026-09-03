de54eecf20fa17a26b24f62deb660b2ba285ee8a
FanghongGuo <fhguo@zjut.edu.cn>
2026-09-03 14:56:45 +0800

    Update publications.js

--- assets/publications.js
+++ assets/publications.js
@@ -39,11 +39,5 @@ window.PUBLICATIONS = [
 }`,
     heroImage: 'assets/images/papers/SDFPCP/SDFPCP.png',
     heroCaption: 'Overall framework of the proposed sequential decentralized federated progressive channel pruning strategy.',
-    gallery: [
-      {
-        src: 'assets/images/papers/SDFPCP/',
-        caption: 'Representative results and analysis for the proposed SDFPCP strategy.'
-      }
-    ]
   }
 ];
